614da69dd76e29327d8a67d54398d23d1a1a29ae
manjur0 <sfmanjur21@gmail.com>
2025-03-20 05:45:22 +0600

    Add About and Projects pages components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.jsx";
+import Projects from "./Pages/Projects.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  </BrowserRouter>
+);
